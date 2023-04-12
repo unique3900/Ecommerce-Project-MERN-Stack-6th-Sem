d9d4029f944f714c0c186f19a0d52d83a8dbb282
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-04-12 15:55:39 +0545

    user-info basic structure made

--- Client/src/user/UserInfo.jsx
+++ Client/src/user/UserInfo.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { useAuth } from '../components/Context-State/auth'
 
 const UserInfo = () => {
-  return (
-    <div>
-      <h3>Namaste</h3>
-    </div>
+     const[auth,setAuth] = useAuth();
+    return (
+      <>
+                    <h3 className='text-3xl text-center font-bold'>User Section</h3>
+        
+            <div className='grid grid-rows-1 items-center gap-3 justify-center lg:grid-cols-20/80 lg:justify-between lg:items-start  mt-4 mx-5'> 
+                
+            {/* Actions */}
+                <div className="flex flex-col py-5 bg-slate-100 w-48 shadow-sm items-center">
+                    <h4 className='font-bold text-xl'>Actions</h4>
+                    <button className="text-white bg-red-500 rounded-lg px-2">Change Password</button>
+                </div>
+                
+                {/* User Data */}
+                <div className="bg-slate-200 shadow-md">
+                    <div className="title">
+                        <h4 className='font-bold text-xl border-black text-center '>User Information's</h4> 
+                    </div>
+                    <div className=" flex flex-col justify-center">
+                        {
+                            <>
+                                 <h2>Name:{JSON.stringify(auth.user.name)}</h2>
+                            <h2>Phone:{JSON.stringify(auth.user.phone) }</h2>
+                            <h2>Email:{JSON.stringify(auth.user.email) }</h2>
+                            <h2>Address:{JSON.stringify(auth.user.address) }</h2>
+                            </>
+                       
+                            
+                            }
+                    </div>
+                       
+
+
+                </div>
+              
+                    </div>
+            </>
   )
 }
 
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns:
+      {
+        '20/80': '20% 80%',
+        'fixed': '40px 260px',
+      },
+      
+    },
   },
   plugins: [],
 }
